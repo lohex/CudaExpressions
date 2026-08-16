3e711fcc3ce6b5d9c2052d55e1295e8cd94499a9
Lorenz Hexemer <67096411+lohex@users.noreply.github.com>
2026-08-16 23:59:44 +0200

    Remove legacy spline exports

--- src/kernex/spline/__init__.py
+++ src/kernex/spline/__init__.py
@@ -1,12 +1,10 @@
-from .api import GPUSpline, GPUSplineVector, Spline, SplineCollection
+from .api import Spline, SplineCollection
 from .compile import Extrapolation, SplineData
 from .inline import evaluate_spline_collection_inline, evaluate_spline_inline
 
 __all__ = [
     "Spline",
     "SplineCollection",
-    "GPUSpline",
-    "GPUSplineVector",
     "SplineData",
     "Extrapolation",
     "evaluate_spline_inline",
