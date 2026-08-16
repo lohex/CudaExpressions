4918ea31799ea6584094242338dca3c82316c552
Lorenz Hexemer <67096411+lohex@users.noreply.github.com>
2026-08-16 23:59:38 +0200

    Remove legacy expression exports

--- src/kernex/expression/__init__.py
+++ src/kernex/expression/__init__.py
@@ -1,11 +1,9 @@
-from .api import Expression, ExpressionVector, GPUExpression, GPUExpressionVector
+from .api import Expression, ExpressionVector
 from .inline import evaluate_expression_inline, evaluate_expression_vector_inline
 
 __all__ = [
     "Expression",
     "ExpressionVector",
-    "GPUExpression",
-    "GPUExpressionVector",
     "evaluate_expression_inline",
     "evaluate_expression_vector_inline",
 ]
