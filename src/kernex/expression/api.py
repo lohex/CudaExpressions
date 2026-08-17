cd5c21e8d2eb93ffafe20f004c89ea5fb1048374
Lorenz Hexemer <67096411+lohex@users.noreply.github.com>
2026-08-17 11:17:05 +0200

    Clean up expression API

--- src/kernex/expression/api.py
+++ src/kernex/expression/api.py
@@ -1,6 +1,4 @@
 """Public high-level expression API."""
-from __future__ import annotations
-
 from collections.abc import Sequence
 
 import numpy as np
@@ -30,36 +28,27 @@ class Expression:
         )
 
     @property
-    def tensor(self) -> np.ndarray:
+    def bytecode(self) -> np.ndarray:
         return self.ir.to_array()
 
     def to_device(self):
-        return cuda_backend.to_device(self.tensor)
+        return cuda_backend.to_device(self.bytecode)
 
     def evaluate(
         self,
-        parameters: np.ndarray,
-        variables: np.ndarray | None = None,
+        parameters,
+        variables=None,
         *,
         threads_per_block: int = 256,
     ) -> np.ndarray:
         return cuda_backend.evaluate_scalar(
-            self.tensor, parameters, variables, threads_per_block=threads_per_block
+            self.bytecode, parameters, variables, threads_per_block=threads_per_block
         )
 
     def to_numpy(self, *, on_array: bool = True):
         return numpy_backend.scalar_function(
             self.base_expression, self.parameters, self.variables, on_array=on_array
         )
-
-    def toTensor(self, on_device: bool = True):
-        return self.to_device() if on_device else self.tensor
-
-    def toNumpy(self, on_array: bool = True):
-        return self.to_numpy(on_array=on_array)
-
-    def eval(self, params, variables=None, threadsperblock: int = 256):
-        return self.evaluate(params, variables, threads_per_block=threadsperblock)
 
 
 class ExpressionVector:
@@ -85,15 +74,15 @@ class ExpressionVector:
         return self.ir.output_dim
 
     @property
-    def tensor(self) -> np.ndarray:
+    def bytecode(self) -> np.ndarray:
         return self.ir.to_array()
 
     def to_device(self):
-        return cuda_backend.to_device(self.tensor)
+        return cuda_backend.to_device(self.bytecode)
 
     def evaluate(self, parameters, variables=None, *, threads_per_block: int = 256):
         return cuda_backend.evaluate_vector(
-            self.tensor,
+            self.bytecode,
             self.output_dim,
             parameters,
             variables,
@@ -104,12 +93,3 @@ class ExpressionVector:
         return numpy_backend.vector_function(
             self.base_expressions, self.parameters, self.variables, on_array=on_array
         )
-
-    def toTensor(self, on_device: bool = True):
-        return self.to_device() if on_device else self.tensor
-
-    def toNumpy(self, on_array: bool = True):
-        return self.to_numpy(on_array=on_array)
-
-    def eval(self, params, variables=None, threadsperblock: int = 256):
-        return self.evaluate(params, variables, threads_per_block=threadsperblock)
